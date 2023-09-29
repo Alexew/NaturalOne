650e289fd39e1bdb6f8fcd5f8a90b0126109a03d
Alexew <gauba.alexew@gmail.com>
2023-09-29 17:34:34 +0300

    Add Fluxor

--- NaturalOne/Client/Program.cs
+++ NaturalOne/Client/Program.cs
@@ -1,3 +1,4 @@
+using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -15,6 +16,7 @@ namespace NaturalOne
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+            builder.Services.AddFluxor(fo => fo.ScanAssemblies(typeof(Program).Assembly));
 
             await builder.Build().RunAsync();
         }
