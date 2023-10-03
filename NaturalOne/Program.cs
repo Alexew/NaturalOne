2cddd3cc665510bd60bb5eb986ad970f36b8f507
Alexew <gauba.alexew@gmail.com>
2023-10-03 17:25:21 +0300

    Add blazored local storage

--- NaturalOne/Program.cs
+++ NaturalOne/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -16,6 +17,7 @@ namespace NaturalOne
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddFluxor(fo => fo.ScanAssemblies(typeof(Program).Assembly));
 
             await builder.Build().RunAsync();
