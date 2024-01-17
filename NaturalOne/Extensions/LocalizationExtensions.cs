559491f399328f3004b669fc20aa891423c67562
Alexew <gauba.alexew@gmail.com>
2024-01-17 14:29:14 +0200

    Add extensions for lists, enums

---
+++ NaturalOne/Extensions/LocalizationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Localization;
+using NaturalOne.Localization;
+
+namespace NaturalOne
+{
+    public static class LocalizationExtensions
+    {
+        public static IStringLocalizer T { get; set; } = Localizer.Instance;
+
+        public static string GetLocalizedEnum<TEnum>(this TEnum enumValue)
+            where TEnum : struct
+        {
+            var type = typeof(TEnum);
+            var enumName = type.Name;
+
+            var enumValueStr = enumValue.ToString();
+            var resourceName = "Enums." + enumName + '.' + enumValueStr;
+
+            return T[resourceName];
+        }
+    }
+}
---
+++ NaturalOne/Extensions/SelectListHelper.cs
@@ -0,0 +1,27 @@
+﻿using NaturalOne.Models;
+
+namespace NaturalOne
+{
+    public static class SelectListHelper
+    {
+        public static IList<SelectListItem> ToSelectList<TEnum>(TEnum? except = null)
+            where TEnum : struct
+        {
+            var list = new List<SelectListItem>();
+
+            foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
+            {
+                if (except is not null && except.Value.Equals(value))
+                    continue;
+
+                list.Add(new SelectListItem
+                {
+                    Text = value.GetLocalizedEnum(),
+                    Value = Convert.ToInt32(value).ToString()
+                });
+            }
+
+            return list;
+        }
+    }
+}
