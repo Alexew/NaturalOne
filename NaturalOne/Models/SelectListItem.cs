e043443e1ded43f714e87a9a7789d1ec1ee01c80
Alexew <gauba.alexew@gmail.com>
2024-01-17 14:28:37 +0200

    Add trait editor model

---
+++ NaturalOne/Models/SelectListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace NaturalOne.Models
+{
+    public class SelectListItem
+    {
+        public string Text { get; set; } = default!;
+        public string Value { get; set; } = default!;
+    }
+}
---
+++ NaturalOne/Models/TraitEditorTemplate.cs
@@ -0,0 +1,21 @@
+﻿namespace NaturalOne.Models
+{
+    public class TraitEditorTemplate
+    {
+        public IList<TraitEditor> Editors { get; set; } = new List<TraitEditor>();
+    }
+
+    public class TraitEditor
+    {
+        public TraitEditor(string id, string name)
+        {
+            Id = id;
+            Name = name;
+            SelectItems = new List<SelectListItem>();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IList<SelectListItem> SelectItems { get; set; }
+    }
+}
